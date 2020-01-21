ace61c9e836218d9af0ab2ffd8f68ee6d57314c9
noferini <noferini@bo.infn.it>
2020-01-21 12:47:07 +0100

    add tof windowfiller util

--- Detectors/TOF/base/src/TOFBaseLinkDef.h
+++ Detectors/TOF/base/src/TOFBaseLinkDef.h
@@ -19,5 +19,6 @@
 #pragma link C++ class vector < o2::tof::Digit> + ;
 #pragma link C++ class vector < vector < o2::tof::Digit>> + ;
 #pragma link C++ class o2::tof::Strip + ;
+#pragma link C++ class o2::tof::WindowFiller + ;
 
 #endif
