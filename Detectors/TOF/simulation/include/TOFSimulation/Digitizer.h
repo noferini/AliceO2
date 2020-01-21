ed2e244d69438ba1c9b3ec6b1464bdc459e785e7
noferini <noferini@bo.infn.it>
2020-01-21 12:47:07 +0100

    add tof windowfiller util

--- Detectors/TOF/simulation/include/TOFSimulation/Digitizer.h
+++ Detectors/TOF/simulation/include/TOFSimulation/Digitizer.h
@@ -14,6 +14,7 @@
 #include "TOFBase/Geo.h"
 #include "TOFBase/Digit.h"
 #include "TOFBase/Strip.h"
+#include "TOFBase/WindowFiller.h"
 #include "TOFSimulation/Detector.h"
 #include "SimulationDataFormat/MCTruthContainer.h"
 #include "TOFSimulation/MCLabel.h"
@@ -24,12 +25,12 @@ namespace o2
 namespace tof
 {
 
-class Digitizer
+class Digitizer : public WindowFiller
 {
   using CalibApi = o2::tof::CalibTOFapi;
 
  public:
-  Digitizer(Int_t mode = 0) : mMode(mode) { init(); };
+ Digitizer(Int_t mode = 0) : WindowFiller(), mMode(mode) { init(); };
   ~Digitizer() = default;
 
   void init();
@@ -44,13 +45,10 @@ class Digitizer
   Float_t getEffZ(Float_t z);
   Float_t getFractionOfCharge(Float_t x, Float_t z);
 
-  Int_t getCurrentReadoutWindow() const { return mReadoutWindowCurrent; }
-  void setCurrentReadoutWindow(Double_t value) { mReadoutWindowCurrent = value; }
   Float_t getTimeLastHit(Int_t idigit) const { return 0; }
   Float_t getTotLastHit(Int_t idigit) const { return 0; }
   Int_t getXshift(Int_t idigit) const { return 0; }
   Int_t getZshift(Int_t idigit) const { return 0; }
-  void setEventTime(double value) { mEventTime = value; }
   void setEventID(Int_t id) { mEventID = id; }
   void setSrcID(Int_t id) { mSrcID = id; }
 
@@ -67,10 +65,6 @@ class Digitizer
   void fillOutputContainer(std::vector<Digit>& digits);
   void flushOutputContainer(std::vector<Digit>& digits); // flush all residual buffered data
 
-  void setContinuous(bool val) { mContinuous = val; }
-  bool isContinuous() const { return mContinuous; }
-
-  std::vector<std::vector<Digit>>* getDigitPerTimeFrame() { return &mDigitsPerTimeFrame; }
   std::vector<o2::dataformats::MCTruthContainer<o2::MCCompLabel>>* getMCTruthPerTimeFrame() { return &mMCTruthOutputContainerPerTimeFrame; }
 
   void setCalibApi(CalibApi* calibApi) { mCalibApi = calibApi; }
@@ -95,34 +89,20 @@ class Digitizer
   Float_t mEffBoundary3;
 
   // info TOF timewindow
-  Int_t mReadoutWindowCurrent = 0;
-  Double_t mEventTime;
   Int_t mEventID = 0;
   Int_t mSrcID = 0;
-
-  bool mContinuous = false;
 
   // digit info
   //std::vector<Digit>* mDigits;
 
-  static const int MAXWINDOWS = 2; // how many readout windows we can buffer
-
-  std::vector<std::vector<Digit>> mDigitsPerTimeFrame;
   std::vector<o2::dataformats::MCTruthContainer<o2::MCCompLabel>> mMCTruthOutputContainerPerTimeFrame;
 
-  int mIcurrentReadoutWindow = 0;
   o2::dataformats::MCTruthContainer<o2::tof::MCLabel> mMCTruthContainer[MAXWINDOWS];
   o2::dataformats::MCTruthContainer<o2::tof::MCLabel>* mMCTruthContainerCurrent = &mMCTruthContainer[0]; ///< Array for MCTruth information associated to digits in mDigitsArrray.
   o2::dataformats::MCTruthContainer<o2::tof::MCLabel>* mMCTruthContainerNext[MAXWINDOWS - 1];            ///< Array for MCTruth information associated to digits in mDigitsArrray.
   o2::dataformats::MCTruthContainer<o2::MCCompLabel>* mMCTruthOutputContainer;
 
-  // array of strips to store the digits per strip (one for the current readout window, one for the next one)
-  std::vector<Strip> mStrips[MAXWINDOWS];
-  std::vector<Strip>* mStripsCurrent = &(mStrips[0]);
-  std::vector<Strip>* mStripsNext[MAXWINDOWS - 1];
-
   // arrays with digit and MCLabels out of the current readout windows (stored to fill future readout window)
-  std::vector<Digit> mFutureDigits;
   std::vector<int> mFutureIevent;
   std::vector<int> mFutureIsource;
   std::vector<int> mFutureItrackID;
@@ -138,23 +118,6 @@ class Digitizer
                 Int_t trackID);
 
   void checkIfReuseFutureDigits();
-
-  bool isMergable(Digit digit1, Digit digit2)
-  {
-    if (digit1.getChannel() != digit2.getChannel()) {
-      return false;
-    }
-
-    if (digit1.getBC() != digit2.getBC()) {
-      return false;
-    }
-
-    // Check if the difference is larger than the TDC dead time
-    if (std::abs(digit1.getTDC() - digit2.getTDC()) > Geo::DEADTIMETDC) {
-      return false;
-    }
-    return true;
-  }
 
   ClassDefNV(Digitizer, 1);
 };
