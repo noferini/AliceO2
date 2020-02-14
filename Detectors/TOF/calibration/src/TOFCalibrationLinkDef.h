c2b49236fd9288e5e96b217d85c748fcfb0f8ae5
Chiara Zampolli <Chiara.Zampolli@cern.ch>
2020-02-14 11:44:53 +0100

    Basic time-slice calibration steering

--- Detectors/TOF/calibration/src/TOFCalibrationLinkDef.h
+++ Detectors/TOF/calibration/src/TOFCalibrationLinkDef.h
@@ -16,4 +16,9 @@
 
 #pragma link C++ class o2::tof::CalibTOFapi + ;
 
+#pragma link C++ class o2::tof::LHCClockDataHisto + ;
+#pragma link C++ class o2::calibration::TimeSlot < o2::tof::LHCClockDataHisto> + ;
+#pragma link C++ class o2::calibration::TimeSlotCalibration < o2::dataformats::CalibInfoTOF, o2::tof::LHCClockDataHisto> + ;
+#pragma link C++ class o2::tof::LHCClockCalibrator + ;
+
 #endif
